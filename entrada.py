94bed9c677694ea7dea754e5335b712703e82b9f
valebmss <leidyvale244@gmail.com>
2024-09-09 15:06:38 -0500

    palabras

--- entrada.py
+++ entrada.py
@@ -1,10 +1,17 @@
-class Animal(object):
- makes_noise:bool = False
- def make_noise(self: "Animal") -> object:
- if (self.makes_noise):
-  a=1+14
-  for For
-  #8knln
- print(self.sound())
- def sound(self: "Animal") -> str:
- return "???"
+import mat
+
+x = 5
+f0r in range(5):    # Error
+    print("ola")
+    x++
+hola = []
+if x > 3:
+    print("Greater than 3")
+else:
+    print("Less than or equal to 3")
+for i in range(10):
+    print(i)
+    hola.append(i)
+ola=90.00.50
+#comillas simples
+print('hola')
